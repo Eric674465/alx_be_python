ed464071e9dfd65e49aca2691dde918ba67ee6cb
Eric674465 <adueric612@gmail.com>
2024-12-18 22:13:23 +0000

    changes to initial one

---
+++ basic_operation.py
@@ -0,0 +1,17 @@
+number1 = 10
+number2 = 5
+
+# Addition
+
+y = number1 + number2
+print("The sum of number1 and number2 is:", y)
+
+# Performing subtraction
+
+x = number1 - number2
+print("The difference between number1 and nmuber2 is:",  x)
+
+# Product of number1 and number2
+
+z = number1 * number2
+print("The product of number1 and number2 is:", z)
